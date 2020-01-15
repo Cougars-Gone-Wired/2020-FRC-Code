6c1bc23dbdfdaa49e411c97602dbd97b54487233
ElSenor64 <sillynoodle36@gmail.com>
2020-01-14 20:44:50 -0700

    Added commented code for accessing game data (for color wheel).

--- src/main/java/frc/robot/ColorWheel.java
+++ src/main/java/frc/robot/ColorWheel.java
@@ -44,3 +44,31 @@ public class ColorWheel {
 
 
 }
+
+// import edu.wpi.first.wpilibj.DriverStation;
+
+// String gameData;
+// gameData = DriverStation.getInstance().getGameSpecificMessage();
+// if(gameData.length() > 0)
+// {
+//   switch (gameData.charAt(0))
+//   {
+//     case 'B' :
+//       //Blue case code
+//       break;
+//     case 'G' :
+//       //Green case code
+//       break;
+//     case 'R' :
+//       //Red case code
+//       break;
+//     case 'Y' :
+//       //Yellow case code
+//       break;
+//     default :
+//       //This is corrupt data
+//       break;
+//   }
+// } else {
+//   //Code for no data received yet
+// }
