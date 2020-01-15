45da4f29de381a909bd44dd7e092d773d98ebf59
CarterPH <carterpherman@gmail.com>
2020-01-14 18:13:45 -0700

    Added spaces

--- src/main/java/frc/robot/ColorWheel.java
+++ src/main/java/frc/robot/ColorWheel.java
@@ -8,17 +8,21 @@ public class ColorWheel {
  
     private static final double COLORWHEEL_SPEED = .4;
     private WPI_TalonSRX wheelMotor;
+
     public ColorWheel() {
         wheelMotor = new WPI_TalonSRX(0);
         currentState = States.NOT_MOVING;
     }
+
     public void initialize() {
         wheelMotor.set(0);
         currentState = States.NOT_MOVING;
     }
+
     public enum States {
         NOT_MOVING, SPINNING
     }
+
     private States currentState;
     public void spin(boolean spinButton) {
         switch(currentState) {
@@ -28,6 +32,7 @@ public class ColorWheel {
                     currentState = States.SPINNING;
                 }
                 break;
+                
             case SPINNING:
                 if (!spinButton) {
                     wheelMotor.set(0);
