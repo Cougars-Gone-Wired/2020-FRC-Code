2d24a8b1671c7aeba5453b7877cb8a6686c815db
ElSenor64 <sillynoodle36@gmail.com>
2020-01-18 13:13:07 -0700

    Started on Climber code.

--- src/main/java/frc/robot/Climber.java
+++ src/main/java/frc/robot/Climber.java
@@ -1,6 +1,9 @@
 package frc.robot;
-// import com.ctre.phoenix.motorcontrol.NeutralMode;
-// import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+// import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class Climber {
-    
+    private WPI_TalonSRX 
 }
