16f225d2ad4140b900fcd719d7e22e2bea423353
Judd <juddbrau@gmail.com>
2020-01-20 14:48:22 -0700

    replaced SRX with FX and added space (My 2 cents on the shooter class)

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -1,46 +1,46 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class Shooter {
-	private static final double SHOOTER_SPEED = 1;
+    private static final double SHOOTER_SPEED = 1;
 
-	private WPI_TalonSRX shooterMotor;
-	
-	private boolean shooterTriggerBool;
+    private WPI_TalonFX shooterMotor;
 
-	public Shooter() {
-		shooterMotor = new WPI_TalonSRX(Constants.SHOOTER_MOTOR_ID);
-		initialize();
-	}
+    private boolean shooterTriggerBool;
 
-	public void initialize() {
-		shooterMotor.set(0);
-		currentShooterState = ShooterStates.NOT_MOVING;
-	}
+    public Shooter() {
+        shooterMotor = new WPI_TalonFX(Constants.SHOOTER_MOTOR_ID);
+        initialize();
+    }
 
-	public enum ShooterStates {
-		NOT_MOVING, SPINNING
-	}
+    public void initialize() {
+        shooterMotor.set(0);
+        currentShooterState = ShooterStates.NOT_MOVING;
+    }
 
-	private ShooterStates currentShooterState;
+    public enum ShooterStates {
+        NOT_MOVING, SPINNING
+    }
 
-	public void shoot(double shooterTrigger) {
-		shooterTriggerBool = (shooterTrigger >= Constants.DEADZONE);
-		
-		switch (currentShooterState) {
-			case NOT_MOVING:
-				if (shooterTriggerBool) {
-					shooterMotor.set(SHOOTER_SPEED);
-					currentShooterState = ShooterStates.SPINNING;
-				}
-				break;
-			case SPINNING:
-				if (!shooterTriggerBool) {
-					shooterMotor.set(0);
-					currentShooterState = ShooterStates.NOT_MOVING;
-				}
-				break;
-		}
-	}
+    private ShooterStates currentShooterState;
+
+    public void shoot(double shooterTrigger) {
+        shooterTriggerBool = (shooterTrigger >= Constants.DEADZONE);
+
+        switch (currentShooterState) {
+        case NOT_MOVING:
+            if (shooterTriggerBool) {
+                shooterMotor.set(SHOOTER_SPEED);
+                currentShooterState = ShooterStates.SPINNING;
+            }
+            break;
+        case SPINNING:
+            if (!shooterTriggerBool) {
+                shooterMotor.set(0);
+                currentShooterState = ShooterStates.NOT_MOVING;
+            }
+            break;
+        }
+    }
 }
