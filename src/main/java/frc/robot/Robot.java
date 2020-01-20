4f498876449dcf990e51bfa037964438e74c0534
Ethan Shahan <tieguy10501>
2020-01-20 13:15:48 -0700

    Fixed indenting formatting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,66 +5,66 @@ import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
-  private Controllers controllers;
-  private Drive drive;
-  private Climber climber;
-  private Shooter shooter;
-  private Intake intake;
-  private Feeder feeder;
-  private Arm arm;
+    private Controllers controllers;
+    private Drive drive;
+    private Climber climber;
+    private Shooter shooter;
+    private Intake intake;
+    private Feeder feeder;
+    private Arm arm;
 
-  @Override
-  public void robotInit() {
-    controllers = new Controllers();
-    drive = new Drive();
-    climber = new Climber();
-    shooter = new Shooter();
-    intake = new Intake();
-    feeder = new Feeder();
-    arm = new Arm();
-  }
+    @Override
+    public void robotInit() {
+        controllers = new Controllers();
+        drive = new Drive();
+        climber = new Climber();
+        shooter = new Shooter();
+        intake = new Intake();
+        feeder = new Feeder();
+        arm = new Arm();
+    }
 
-  @Override
-  public void robotPeriodic() {
-  }
+    @Override
+    public void robotPeriodic() {
+    }
 
-  @Override
-  public void autonomousInit() {
-  }
+    @Override
+    public void autonomousInit() {
+    }
 
-  @Override
-  public void autonomousPeriodic() {
-  }
+    @Override
+    public void autonomousPeriodic() {
+    }
 
-  @Override
-  public void teleopInit() {
-    drive.initalize();
-    climber.initalize();
-    shooter.initialize();
-    intake.initialize();
-    feeder.initialize();
-    arm.initialize();
-  }
+    @Override
+    public void teleopInit() {
+        drive.initalize();
+        climber.initalize();
+        shooter.initialize();
+        intake.initialize();
+        feeder.initialize();
+        arm.initialize();
+    }
 
-  @Override
-  public void teleopPeriodic() {
-    controllers.updateControllerValues();
-    
-    // Mobility
-    drive.robotDrive(controllers.getDriveSpeedAxis(), controllers.getDriveTurnAxis());
-    drive.setSide(controllers.driveSideToggle.getValue());
-    climber.climb(controllers.getClimberUpTrigger(), controllers.getClimberDownTrigger());
+    @Override
+    public void teleopPeriodic() {
+        controllers.updateControllerValues();
 
-    // Manipulator
-    shooter.shoot(controllers.getShooterTrigger());
-    intake.intake(controllers.getIntakeTrigger());
-    intake.intakeArm(controllers.intakeArmToggle.getValue());
-    feeder.feed(controllers.isFeederButton());
-    arm.pistonArm(controllers.isShootingPosButton(), controllers.isStartingPosButton(),
-        controllers.isClimbingPosButton());
-  }
+        // Mobility
+        drive.robotDrive(controllers.getDriveSpeedAxis(), controllers.getDriveTurnAxis());
+        drive.setSide(controllers.driveSideToggle.getValue());
+        climber.climb(controllers.getClimberUpTrigger(), controllers.getClimberDownTrigger());
 
-  @Override
-  public void testPeriodic() {
-  }
+        // Manipulator
+        shooter.shoot(controllers.getShooterTrigger());
+        intake.intake(controllers.getIntakeTrigger());
+        intake.intakeArm(controllers.intakeArmToggle.getValue());
+        feeder.feed(controllers.isFeederButton());
+        arm.pistonArm(controllers.isShootingPosButton(), controllers.isStartingPosButton(),
+                controllers.isClimbingPosButton());
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
 }
