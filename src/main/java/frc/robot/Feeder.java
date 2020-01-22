876431e835e9b69e13bdfd2f08b2b3d213750ddd
CarterPH <carterpherman@gmail.com>
2020-01-21 17:42:13 -0700

    Changed to yaxis instead of buttons and added outtake.

--- src/main/java/frc/robot/Feeder.java
+++ src/main/java/frc/robot/Feeder.java
@@ -17,22 +17,33 @@ public class Feeder {
     }
 
     public enum States {
-        NOT_MOVING, FEEDING
+        NOT_MOVING, FEEDING, OUTTAKING
     }
 
     private States currentState;
 
-    public void feed(boolean feedButton) {
+    public void feed(double feederAxis) {
         switch(currentState) {
             case NOT_MOVING:
-                if (feedButton) {
+                if (feederAxis >= Constants.DEADZONE) {
                     feederMotor.set(FEEDER_SPEED);
                     currentState = States.FEEDING;
                 }
+                if (feederAxis <= -Constants.DEADZONE) {
+                    feederMotor.set(-FEEDER_SPEED);
+                    currentState = States.OUTTAKING;
+                }
                 break;
-            
+
             case FEEDING:
-                if (!feedButton) {
+                if (feederAxis < Constants.DEADZONE) {
+                    feederMotor.set(0);
+                    currentState = States.NOT_MOVING;
+                }
+                break;
+
+            case OUTTAKING:
+                if (feederAxis > -Constants.DEADZONE) {
                     feederMotor.set(0);
                     currentState = States.NOT_MOVING;
                 }
