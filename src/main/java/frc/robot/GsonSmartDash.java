04da5d6887b6371573a581e1c7b0d750b293c099
Judd <juddbrau@gmail.com>
2020-01-25 13:20:07 -0700

    Added switchSide and limelight button recorders ( and SPACES)

--- src/main/java/frc/robot/GsonSmartDash.java
+++ src/main/java/frc/robot/GsonSmartDash.java
@@ -4,16 +4,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class GsonSmartDash {
 
-	static boolean shouldRecord;
-	static String gsonFileName;
+    static boolean shouldRecord;
+    static String gsonFileName;
 
-	public static void put() {
-		SmartDashboard.putBoolean("Should Record", false);
-		SmartDashboard.putString("Gson File Name", "");
-	}
+    public static void put() {
+        SmartDashboard.putBoolean("Should Record", false);
+        SmartDashboard.putString("Gson File Name", "");
+    }
 
-	public static void set() {
-		shouldRecord = SmartDashboard.getBoolean("Should Record", false);
-		gsonFileName = SmartDashboard.getString("Gson File Name", "notGood");
-	}
+    public static void set() {
+        shouldRecord = SmartDashboard.getBoolean("Should Record", false);
+        gsonFileName = SmartDashboard.getString("Gson File Name", "notGood");
+    }
 }
--- src/main/java/frc/robot/StateRecorder.java
+++ src/main/java/frc/robot/StateRecorder.java
@@ -5,22 +5,24 @@ import java.util.List;
 
 public class StateRecorder {
 
-	List<State> states;
+    List<State> states;
 
-	public void initialize() {
-		states = new ArrayList<>();
-	}
+    public void initialize() {
+        states = new ArrayList<>();
+    }
 
-	public void record(Controllers controllers) {
-		State s = new State();
+    public void record(Controllers controllers) {
+        State s = new State();
 
-		s.setDriveSpeedAxisState(controllers.getDriveSpeedAxis());
-		s.setDriveTurnAxisState(controllers.getDriveTurnAxis());
+        s.setDriveSpeedAxisState(controllers.getDriveSpeedAxis());
+        s.setDriveTurnAxisState(controllers.getDriveTurnAxis());
+        s.setSwitchSideState(controllers.isDriveSideToggle());
+        s.setLimelightState(controllers.getLimelightButton());
 
-		states.add(s);
-	}
+        states.add(s);
+    }
 
-	public List<State> getStates() {
-		return states;
-	}
+    public List<State> getStates() {
+        return states;
+    }
 }
