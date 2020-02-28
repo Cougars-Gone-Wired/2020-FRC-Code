79cad303cbe766110357a30ba83d047c155d6cc4
Tieguy01 <tieguy10501@gmail.com>
2020-02-28 09:11:46 -0700

    Added drive straight to auto selector

--- src/main/java/frc/robot/AutoSelector.java
+++ src/main/java/frc/robot/AutoSelector.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.commands.DoNothing;
 import frc.robot.commands.DriveStraight;
+import frc.robot.commands.ProfileDrive;
 import frc.robot.commands.autoPrograms.IntakeThroughTrench;
 import frc.robot.commands.autoPrograms.OfflineAndShoot;
 import frc.robot.commands.autoPrograms.ShootAndOffLine;
@@ -14,7 +15,7 @@ import frc.robot.commands.autoPrograms.ShootFromTrench;
 public class AutoSelector {
 
     public enum Programs {
-        DRIVE_OFF_LINE, SHOOT_AND_OFF_LINE, SHOOT_FROM_TRENCH, INTAKE_THROUGH_TRENCH, DO_NOTHING
+        DRIVE_OFF_LINE, SHOOT_AND_OFF_LINE, SHOOT_FROM_TRENCH, INTAKE_THROUGH_TRENCH, DRIVE_STRAIGHT, DO_NOTHING
     }
 
     private SendableChooser<Programs> autoChooser = new SendableChooser<>(); // sendable chooser to hold dead reckoning
@@ -30,6 +31,7 @@ public class AutoSelector {
         autoChooser.addOption("Shoot and off line", Programs.SHOOT_AND_OFF_LINE);
         autoChooser.addOption("Shoot from trench", Programs.SHOOT_FROM_TRENCH);
         autoChooser.addOption("Intake Through Trench", Programs.INTAKE_THROUGH_TRENCH);
+        autoChooser.addOption("Drive Straight", Programs.DRIVE_STRAIGHT);
         autoChooser.addOption("Do Nothing", Programs.DO_NOTHING);
         SmartDashboard.putData("Auto", autoChooser);
     }
@@ -52,6 +54,9 @@ public class AutoSelector {
 
             case DRIVE_OFF_LINE:
                 return new DriveStraight(DriveConstants.DRIVE_OFF_LINE_DISTANCE);
+
+            case DRIVE_STRAIGHT:
+                return new ProfileDrive(Robot.drive).getProfilingCommand("paths/output/1MeterBack.wpilib.json");
 
             case DO_NOTHING:
                 return new DoNothing();
