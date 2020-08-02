c9849c844fd7c902bd58586e5d7d2d3d5296a76a
Tieguy01 <tieguy10501@gmail.com>
2020-08-02 15:32:02 -0600

    Added comments to shooter class

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -9,16 +9,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter {
     public static double SHOOTER_SPEED = 0.5;//.47
+
+    // all of these constants need to be tuned
     public static double F = 0.044;
     public static double P = 0.9;//.6
     public static double I = 0.001;
-    public static int IZONE = 1400;
+    public static int IZONE = 1400; // distance from desired velocity at which I kicks in
     public static double D = 11;
-    public static double VOLTAGE_TO_VELOCITY = 20000;
-    public static double DESIRED_VELOCITY = SHOOTER_SPEED * VOLTAGE_TO_VELOCITY;
+    
+    public static double VOLTAGE_TO_VELOCITY = 20000; // constant to switch between % speed and raw velocity
+    public static double DESIRED_VELOCITY = SHOOTER_SPEED * VOLTAGE_TO_VELOCITY; // goal is to only have to set the shooter speed constant so you don't have to figure out what speed corelates to what velocity
+
+    // voltage mode velocity thresholds depend on the tested average velocity at a given shooter speed, but should be lower so that it is easier to start shooting 
     public static double VOLTAGE_INITIAL_VELOCITY_THRESHOLD = 5;//250
-    public static double PID_INITIAL_VELOCITY_THRESHOLD = 5;
     public static double VOLTAGE_VELOCITY_THRESHOLD = 50;//500
+
+    // PID mode velocity thresholds should ideaklly be closer to the desired velocity so that the shooter only shoots when it is up to speed
+    public static double PID_INITIAL_VELOCITY_THRESHOLD = 5;
     public static double PID_VELOCITY_THRESHOLD = 70;
 
     private WPI_TalonFX shooterMotor;
@@ -55,6 +62,8 @@ public class Shooter {
     public void initShooterMotor() {
         shooterMotor.setInverted(true);
         shooterMotor.setNeutralMode(NeutralMode.Coast);
+
+        // how PID is implemented on the motor controller
         shooterMotor.config_kF(0, F, 10);
         shooterMotor.config_kP(0, P, 10);
         shooterMotor.config_kI(0, I, 10);
@@ -74,6 +83,7 @@ public class Shooter {
         SmartDashboard.putNumber("Velocity Thresh", PID_VELOCITY_THRESHOLD);
     }
 
+    // used for PID tuning
     public void shooterDashboard() {
         setConstants = SmartDashboard.getBoolean("Set Constants", false);
         if (setConstants) {
@@ -103,6 +113,7 @@ public class Shooter {
 
     ShooterModes currentShooterMode;
 
+    // used to switch between controlling the shooter with PID and shooting at a set speed
     public void controlShooter(double shooterTrigger, boolean shooterToggle) {
         
         switch(currentShooterMode) {
@@ -138,6 +149,7 @@ public class Shooter {
 
     private ShooterStates currentShooterState;
 
+    // call the control the shooter with a set speed
     public void controlVoltageShooter(double shooterTrigger) {
         shooterDashboard();
         shooterTriggerBool = (shooterTrigger >= Constants.DEADZONE);
@@ -148,7 +160,7 @@ public class Shooter {
             if (shooterTriggerBool 
                     && !Robot.arms.isArmClimbingPosition()
                     && !Robot.intake.isIntaking()) {
-                setVoltageShooting();
+                setVoltageShooting(); // sets shooter at constant voltage, so only needs to be called once
             }
             break;
 
@@ -162,6 +174,7 @@ public class Shooter {
         }
     }
 
+    // call to control the shooter with PID implementation
     public void controlPIDShooter(double shooterTrigger) {
         shooterDashboard();
         shooterTriggerBool = (shooterTrigger >= Constants.DEADZONE);
@@ -177,7 +190,7 @@ public class Shooter {
                 break;
 
             case SHOOTING:
-                setPIDShooting();
+                setPIDShooting(); // called every cycle to adjust shooter voltage output
                 if (!shooterTriggerBool 
                         || Robot.arms.isArmClimbingPosition()
                         || Robot.intake.isIntaking()) {
@@ -200,44 +213,52 @@ public class Shooter {
         currentShooterState = ShooterStates.NOT_MOVING;
     }
 
+    // call for voltage mode if setting speed to something other than speed constant
     public void setVoltageShooting(double shooterSpeed) {
         this.shooterSpeed = shooterSpeed;
         shooterMotor.set(shooterSpeed);
         currentShooterState = ShooterStates.SHOOTING;
     }
 
+    // call for voltage mode if setting speed to the speed constant
     public void setVoltageShooting() {
         shooterSpeed = SHOOTER_SPEED;
         setVoltageShooting(shooterSpeed);
     }
 
+    // call for PID mode if setting the desired velocity to something other than the desired velocity constant
     public void setPIDShooting(double desiredVelocity) {
         this.desiredVelocity = desiredVelocity;
         updateVelocity();
         shooterDashboard();
-        shooterMotor.set(ControlMode.Velocity, desiredVelocity);
+        shooterMotor.set(ControlMode.Velocity, desiredVelocity); // this is all of the PID implementation, simply set the constants on the motor controller, call this, and that's it
         currentShooterState = ShooterStates.SHOOTING;
     }
 
+    // call for PID mode if setting the desired velocity to the desired velocity constant
     public void setPIDShooting() {
         desiredVelocity = DESIRED_VELOCITY;
         setPIDShooting(desiredVelocity);
     }
 
+    // used to reset the velocity thresholds when switching between shooter modes
     public void setVelocityThresholds(double initialVelocityThreshold, double velocityThreshold) {
         this.initialVelocityThreshold = initialVelocityThreshold;
         this.velocityThreshold = velocityThreshold;
     }
 
+    // call every cycle to update the velocity readings from the shooter motor
     public void updateVelocity() {
         velocity = -sensors.getIntegratedSensorVelocity();
-        error = DESIRED_VELOCITY - velocity;
+        error = DESIRED_VELOCITY - velocity; // what PID is trying to minimize
     }
 
+    // used to indicate when the feeder should start feeding the shooter
     public boolean atInitialDesiredVelocity() {
         return Math.abs(error) <= initialVelocityThreshold;
     }
 
+    // used to indicate when the feeder should stop feeding the shooter (if false)
     public boolean atDesiredVelocity() {
         return Math.abs(error) <= velocityThreshold;
     }
